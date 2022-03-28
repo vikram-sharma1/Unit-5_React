64e3703ea378e07ce034e8323ea56f48eb6871eb
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-03-28 16:53:35 +0530

    undo

--- unit-5/sprint-4/evalution/src/Components/Event/Event.jsx
+++ unit-5/sprint-4/evalution/src/Components/Event/Event.jsx
@@ -1,79 +1,17 @@
 // This is an event details page which has its own route
 
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-
 export const Event = () => {
-  const { id } = useParams();
-  const [event, setEvent] = useState({});
-  // const [user, setUser] = useState({});
-
-  // const checkLogin = () => {};
-
-  useEffect(() => {
-    getEvent();
-  }, []);
-
-  const getEvent = () => {
-    axios.get(`http://localhost:8080/meetups/${id}`).then(({ data }) => {
-      console.log(data);
-      setEvent(data);
-    });
-  };
-
-  // const subscribe = () =>{
-  //   axios.patch(`http://localhost:8080/meetups/${id}`, ).then(({ data }) => {
-  //     console.log(data);
-  //     setEvent(data);
-  //   });
-  // }
-
   return (
     <div className="eventContainer">
       {/* add your children here (divs)
       ex : title, theme, description, date, time, location, image(optional)
       the classNames should be also : title, theme, description, date, time, location, image(optional)
       */}
-      <div className="title">
-        {" "}
-        <h1>{event.title}</h1>{" "}
-      </div>
-      <div className="theme">
-        {" "}
-        <h3>{event.theme}</h3>
-      </div>
-      <div className="description">
-        {" "}
-        <h3>{event.description}</h3>
-      </div>
-      <div className="date">
-        {" "}
-        <h3>{event.date}</h3>
-      </div>
-      <div className="time">
-        <h3>{event.time}</h3>{" "}
-      </div>
-      <div className="location">
-        {" "}
-        <h3>{event.location}</h3>{" "}
-      </div>
-      <div className="image">
-        {" "}
-        <img src={event.image} alt="" />
-      </div>
 
       {/* only one of the buttons should be visible depending on the status of subcription
       Hint : use conditional rendering */}
       <button className="unsubscribe">Unsubscribe</button>
-      <button
-        className="subscribe"
-        onClick={() => {
-          // subscribe();
-        }}
-      >
-        Subscribe
-      </button>
+      <button className="subscribe" onClick={() => { }}>Subscribe</button>
     </div>
   );
 };
--- unit-5/sprint-4/evalution/src/Components/Home/Home.jsx
+++ unit-5/sprint-4/evalution/src/Components/Home/Home.jsx
@@ -1,39 +1,13 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-// import { userLogin } from "./";
 
 export const Home = () => {
-  const [arr, setArr] = useState([]);
-
-  const dispatch = useDispatch();
-
-  const { checkUser } = useSelector((store) => store.user);
-
-  // const updateUser = () => {
-  //   if (checkUser.name != undefined) {
-  //     dispatch;
-  //   }
-  // };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = () => {
-    axios.get(`http://localhost:8080/meetups`).then(({ data }) => {
-      setArr([...data]);
-    });
-  };
-
   return (
     <div className="homeContainer">
-      {arr
-        .filter((el) => {}) // Filter on the basis of Users interests and location (both true)
+      {[]
+        .filter((el) => { }) // Filter on the basis of Users interests and location (both true)
         .map((el) => {
           return (
-            <Link to={``} className="events">
+            <Link to={`add route here`} className="events">
               {/* add your children here (divs)
               ex : title, theme, description, date, time, location, image(optional)
               the classNames should be also : title, theme, description, date, time, location, image(optional)
@@ -45,8 +19,8 @@ export const Home = () => {
       <div className="subscribedData">
         <div>
           <select
-            value={"add your value here"} // add value here
-            onChange={(e) => {}}
+            value={"add your value here"}  // add value here
+            onChange={(e) => { }}
           >
             <option value="">------</option>
             <option value="bangalore">Bangalore</option>
@@ -60,17 +34,17 @@ export const Home = () => {
         <div className="subscribedEvents">
           {/* All user subcribed events should be displayed here in an ascending order of date */}
 
-          {arr.map((el) => {
-            return (
-              <Link to={`/meetup/${el.id}`} className="events">
-                {/* Each event should have these elements/children (divs):
+          {[]
+            .map((el) => {
+              return (
+                <Link to={`add route here`} className="events">
+                  {/* Each event should have these elements/children (divs):
                     ex : title, theme, description, date, time, location, image(optional)
                     the classNames should be also : title, theme, description, date, time, location, image(optional) */}
-                <br />
-                {el.title}
-              </Link>
-            );
-          })}
+                </Link>
+              );
+            })}
+
         </div>
       </div>
     </div>
