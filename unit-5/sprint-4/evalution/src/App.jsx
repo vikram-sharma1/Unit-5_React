11ae7517862ba334606dfe1997f1be1f399e3696
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-03-28 14:05:38 +0530

    first

--- unit-5/sprint-4/evalution/src/App.jsx
+++ unit-5/sprint-4/evalution/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { Routers } from "./Components/Routers/Routers";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      Hello
+      <Routers />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
