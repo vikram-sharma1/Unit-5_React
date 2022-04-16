5fcc1ecc47bd79dfeb8ee4c80728d3845b99f0fb
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-04-16 20:08:59 +0530

    changes

--- unit-6/assign/src/components/AddCountry.jsx
+++ unit-6/assign/src/components/AddCountry.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const AddCountry = () => {
+
+const [country, setCountry] = useState("")
+
+let api = `https://localhost:8080/country`
+
+    // const handleChange = (e) => {
+
+    //     const {id,value} = e.target
+    //     setCountry({
+    //         ...country,
+    //         [id]:value
+    //     })
+    // }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post(`${api}`,{name:country}).then(()=>{
+            alert("Country Added")
+            // setCountry({name:""})
+        })
+    }
 
   return (
    
     <h1>
-        <form action="">
+        <form onSubmit={(e)=>handleSubmit(e)}>
             <label>Add Country</label>
                  <br />
-            <input type="text" name="" id="" className='countryNameInp'/>
+            <input type="text" 
+                id={"country"} 
+                value={country.name} 
+                // className='countryNameInp' 
+                onChange={(e)=>setCountry(e.target.value)}  />
                   <br />
-            <input type="submit" className='countryNameInp'/>
+            <input type="submit" value={"Enter"} className='countryNameInp'/>
         </form>
     </h1>
   
