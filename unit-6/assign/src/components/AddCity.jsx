547e1d50a60821a2a11f7f5b49f8d51afc2105a3
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-04-16 18:57:58 +0530

    inputs

--- unit-6/assign/src/components/AddCity.jsx
+++ unit-6/assign/src/components/AddCity.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const AddCity = () => {
-  return (
-    <h1>AddCity</h1>
-  )
+    return (
+   
+        <h2>
+            <p className='Addcity'>Add City</p>
+            <form action="">
+                <label className='cityInp'>City Name</label>
+                <input type="text" name="" id="" className='countryNameInpcity'/>
+                      <br />
+                <label className='cityInp'>Population</label>
+                <input type="text" name="" id="" className='countryNameInpcity'/>
+                      <br />
+                <label className='cityInp'>Country</label>
+                <input type="text" name="" id="" className='countryNameInpcity'/>
+                      <br />
+                      <br/>
+                <input type="submit" className='countryNameInp'/>
+            </form>
+        </h2>
+      
+      )
 }
 
 export default AddCity
--- unit-6/assign/src/components/AddCountry.jsx
+++ unit-6/assign/src/components/AddCountry.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const AddCountry = () => {
 
   return (
-    <h1>AddCountry</h1>
+   
+    <h1>
+        <form action="">
+            <label>Add Country</label>
+                 <br />
+            <input type="text" name="" id="" className='countryNameInp'/>
+                  <br />
+            <input type="submit" className='countryNameInp'/>
+        </form>
+    </h1>
   
   )
 }
