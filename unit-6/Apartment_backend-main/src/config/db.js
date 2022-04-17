e6c0624a2e9addf82511bc52cb94a6d1303d04ef
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-04-17 15:36:59 +0530

    server

--- unit-6/Apartment_backend-main/src/config/db.js
+++ unit-6/Apartment_backend-main/src/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 module.exports = () =>{
 
-    // return mongoose.connect('mongodb+srv://appartment:appartment@cluster0.hla9z.mongodb.net/masai?retryWrites=true&w=majority')
-    return mongoose.connect('mongodb+srv://fullstack:fullstack@cluster0.kzmo8.mongodb.net/DatabaseApartment?retryWrites=true&w=majority')
+    return mongoose.connect('mongodb+srv://appartment:appartment@cluster0.hla9z.mongodb.net/masai?retryWrites=true&w=majority')
 } 
