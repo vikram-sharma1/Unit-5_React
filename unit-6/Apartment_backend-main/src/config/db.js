0908e7bac242a13d62a8bb70320a151baf39664e
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-04-17 15:51:16 +0530

    added .env file

--- unit-6/Apartment_backend-main/src/config/db.js
+++ unit-6/Apartment_backend-main/src/config/db.js
@@ -1,5 +1,6 @@
+
 const mongoose = require('mongoose')
 module.exports = () =>{
-
-    return mongoose.connect('mongodb+srv://appartment:appartment@cluster0.hla9z.mongodb.net/masai?retryWrites=true&w=majority')
+    return mongoose.connect(process.env.MONGO_URL,
+    {useNewUrlParser: true})
 } 
